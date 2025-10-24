22012a1e2b97214a3aa76ab82d90b39f750b514c
ziraqi <126034358+ziraqi@users.noreply.github.com>
2025-10-24 06:30:59 +0000

    improve bio

--- streamlit_CS/pages/1_Bio.py
+++ streamlit_CS/pages/1_Bio.py
@@ -2,19 +2,23 @@ import streamlit as st
 
 st.title("👋 My Bio")
 
-# ---------- TODO: Replace with your own info ----------
-NAME = "Your Name"
-PROGRAM = "Your Program / Major / Role"
+NAME = "Zakaria Iraqi"
+PROGRAM = "MSU Denver Computer Science Bachelor's of Science"
 INTRO = (
-    "Write 2–3 sentences about yourself: what you’re studying/working on, "
-    "what excites you about data visualization or computing, etc."
+    "Hello everyone! I'm Zakaria Iraqi, currently pursuing my Bachelor's degree in Computer Science at MSU Denver. " \
+    "I am a self-motivated and passionate individual with a keen interest in technology and programming. " \
+    "I am currently working on a project that involves building a web application that displays drift detection" \
+    " in a machine learning model using React + Vite for the frontend."
+    "Data Visualization is very exciting because it enables the viewer to analyze separate patters that may not have" \
+    " been obvious in raw data. I am eager to learn more and contribute to the field of computer science" \
+    " through my studies and projects."
 )
 FUN_FACTS = [
-    "I love …",
-    "I’m learning …",
-    "I want to build …",
+    "I love playing basketball and pickleball during my free time.",
+    "I'm learning Spanish to add on to my repertoire of languages and hope to become fluent one day.",
+    "I want to build a tech startup that enables 3rd world countries to have better access to technology.",
 ]
-PHOTO_PATH = "your_photo.jpg"  # Put a file in repo root or set a URL
+PHOTO_PATH = "profile.jpg"  # Put a file in repo root or set a URL
 
 # ---------- Layout ----------
 col1, col2 = st.columns([1, 2], vertical_alignment="center")
